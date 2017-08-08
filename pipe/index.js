f7ccb3c6db59e39c86dc564dc1b159fe0b4ac31f
Ope Adeyomoye <victoradeyomoye@yahoo.co.uk>
2017-08-09 00:17:59 +0100

    🚧 Mild experiments.

--- pipe/index.js
+++ pipe/index.js
@@ -22,7 +22,6 @@ if (!fs.existsSync(tailEnd)) {
     process.exit(0); // @todo: appropriate exit code
 }
 
-
 var file = watcher.watch(opening, {persistent: true});
 
 if (file) {
@@ -66,4 +65,45 @@ function push(source, destination, callback) {
         });
     });
     callback(false); // no errors
+}
+
+function callerFunction() {
+
+}
+
+function watcherFunction() {
+    // starts the watch, non-persistently
+    let updateSuccessful = false;
+    let file = watcher.watch(opening, {persistent: false});
+
+    file.on('ready', path => { console.log(`${path} is now being watched for changes`) })
+        .on('change',path => {
+            console.log(`${path} has been edited`);
+            if (!pushSync(opening, tailEnd)) {
+                console.log('Could not update tail end of the pipe.');
+            } else {
+                console.log('Destination file has been updated');
+                restarterFunction();
+            }
+        });
+    
+    return updateSuccessful;
+}
+
+function restarterFunction() {
+    // calls the caller to start the watch if a condition is met
+}
+
+function pushSync(source, destination) {
+    console.log([source, destination]);
+    let srcStream = fs.readFileSync(source, {encoding: 'utf-8'});
+
+    if (!srcStream) {
+        console.log(srcStream);
+        return false;
+    }
+
+    fs.writeFileSync(destination, srcStream, {encoding: 'utf-8'});
+    delete srcStream;
+    return true;
 }
